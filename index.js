fd57b85895c8b38bce0dbb6db6643380551c7e33
nagendra <nagendrasha191@gmail.com>
2024-10-18 14:49:47 +0530

    routes added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const { default: axios } = require("axios");
+const express = require('express');
+const axios = require('axios');
 
-const getProducts = axios.get('https://dummyjson.com/products').then(Response=>{
-    console.log(Response.data.products);
-}).catch(err=>{
-    console.log(err);
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  // Make the Axios request when the route is accessed
+  axios.get('https://dummyjson.com/products')
+    .then(response => {
+    //   console.log(response.data.products); // Log the products to the console
+      res.send(response.data.products); // Send the products as the response
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('An error occurred while fetching products');
+    });
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
